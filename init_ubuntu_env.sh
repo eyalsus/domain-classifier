1f6af4e74aac55377144bd90f9c760c4c816bf16
Eyal Paz <eyalsus@gmail.com>
2019-09-13 12:27:35 +0300

    bash fix

--- init_ubuntu_env.sh
+++ init_ubuntu_env.sh
@@ -5,8 +5,8 @@ export ASN_DB_PATH="/home/eyalp/domain-classifier/resources"
 export FETCH_SLEEP_TIME=3600
 sudo service redis-server start
 sudo service postgresql start
-nohup python3 /home/eyalp/domain-classifier/enrich_domain.py
-nohup python3 /home/eyalp/domain-classifier/database_connector.py
-nohup python3 /home/eyalp/domain-classifier/fetch_feeds.py --data-source PhishTank --infinity
-nohup python3 /home/eyalp/domain-classifier/fetch_feeds.py --data-source OpenPhish --infinity
+nohup python3 /home/eyalp/domain-classifier/enrich_domain.py &
+nohup python3 /home/eyalp/domain-classifier/database_connector.py &
+nohup python3 /home/eyalp/domain-classifier/fetch_feeds.py --data-source PhishTank --infinity &
+nohup python3 /home/eyalp/domain-classifier/fetch_feeds.py --data-source OpenPhish --infinity &
 #sudo service cron start
